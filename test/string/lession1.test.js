45089be48ff8484770d23d67b8602058b8073b76
alex <xuliang1396@163.com>
2019-08-21 00:27:17 +0800

    leetcode 557

---
+++ test/string/lession1.test.js
@@ -0,0 +1,5 @@
+import revertByWorld from '../../code/string/lession1'
+
+test('revertByWorld:Let\'s take Leetcode contest',()=>{
+    expect(revertByWorld("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
+})
